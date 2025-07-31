b545c4274c6861ebfa9bc1b89ebf56dcc62136d0
jitendra <jitendrakumarbunkar85@gmail.com>
2025-07-31 23:03:47 +0530

    suspisious mail bug solved

--- index.js
+++ index.js
@@ -14,47 +14,29 @@ const ses = new AWS.SES({
 });
 
 const params = {
-  Source: "support@mylamp.in",
+  Source: '"MyLamp Team" <outreach@mail.mylamp.in>',
   Destination: {
-    ToAddresses: ["jitendrakumarbunkar85@gmail.com"],
+    ToAddresses: ["jitu.wize@gmail.com","tagsworldarpit@gmail.com","ashutoshasharsiper@gmail.com","jitendrakumarbunkar85@gmail.com"],
   },
   Message: {
     Subject: {
-      Data: "Your OTP Code to Login",
+      Data: "Outreach testing6...",
       Charset: "UTF-8",
     },
     Body: {
-      Text: {
-        Charset: "UTF-8",
-        Data: `Dear user,
-
-You requested a one-time password (OTP) to log in to your account.
-
-Your OTP is: 593218
-
-This code will expire in 10 minutes. Please do not share this code with anyone.
-
-If you did not request this, please ignore this email.
-
-Thanks,
-Team MyLamp.in`,
-      },
       Html: {
         Charset: "UTF-8",
         Data: `<!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8">
-    <title>OTP Verification</title>
+    <title>Test Verification</title>
   </head>
   <body style="font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px;">
     <div style="max-width: 600px; margin: auto; background: white; padding: 20px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);">
-      <h2 style="color: #333;">Login OTP for MyLamp.in</h2>
+      <img src="https://wize.co.in/images/org_logo.png" width="100px" height="100px" alt="wize_logo" />
       <p>Dear user,</p>
-      <p>You requested a one-time password (OTP) to log in to your account.</p>
-      <p style="font-size: 20px; font-weight: bold; color: #2c3e50; background: #ecf0f1; padding: 10px; display: inline-block; border-radius: 4px;">593218</p>
-      <p>This code will expire in 10 minutes. Please do not share this code with anyone.</p>
-      <p>If you did not request this, please ignore this email.</p>
+      <p>This is testing email , please ignore this email.</p>
       <br>
       <p>Thanks,<br><strong>Team MyLamp.in</strong></p>
     </div>
